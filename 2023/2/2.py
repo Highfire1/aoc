8076b211bd1eedd29061e9bba42e61e7925b2016
Anderson T <tseng.andersonn@gmail.com>
2023-12-01 21:26:52 -0800

    day 2 (significantly easier)

---
+++ 2023/2/2.py
@@ -0,0 +1,57 @@
+with open("input.txt", "r") as fi:
+    data = fi.read().splitlines() 
+
+sum = 0
+
+for line in data:
+    
+    split = line.split(": ")
+    number = int(split[0].split(" ")[1])
+    
+    sets = split[1].split("; ")
+    
+    cubes = {
+        "red" : 0,
+        "green" : 0,
+        "blue" : 0,
+    }
+    
+    for reveals in sets:
+        
+        plays:list[str] = reveals.split(", ")
+        
+        for play in plays:
+            
+            split = play.split(" ")
+            num = int(split[0])
+            color = split[1]
+                        
+            if color in cubes:
+                cubes[color] = max(cubes[color], num)
+            else:
+                cubes[color] = num
+        
+    #print(sets)
+    print(cubes)
+    #print()
+    
+    mult = 1
+    for i in cubes:
+        
+        mult = mult * cubes[i]
+        print(cubes[i])
+    
+    print(mult)   
+    sum += mult 
+    #if cubes["red"] <= 12 and cubes["green"] <= 13 and cubes["blue"] <= 14:
+        #print(number)
+        #sum += number
+        
+print(sum)
+    
+        
+        
+        
+
+    
+    
