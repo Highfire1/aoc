9b7f99346e52743f941bb016e33aef64723ef74c
Anderson T <tseng.andersonn@gmail.com>
2023-12-02 22:34:49 -0800

    refactoring

--- 2023/3/2.py
+++ 2023/3/2.py
@@ -1,13 +1,14 @@
 with open("input.txt", "r") as fi:
     data = fi.read().splitlines() 
 
-#print(data)
+
 
 
 max_i = len(data)
 
 sum = 0
 
+# I LOVE SIDE EFFECTS
 def selectnumbers(row_index, char_index) -> int:
     
     if row_index < 0 or row_index >= len(data):
@@ -21,7 +22,6 @@ def selectnumbers(row_index, char_index) -> int:
     
     for i, char in enumerate(data[row_index]):
         
-        #print(i, char)
         if char.isdecimal():
             selected += char
             
@@ -34,30 +34,21 @@ def selectnumbers(row_index, char_index) -> int:
                 break
         
             selected = ""
-        
-            
-        
-            
+
     
     if selected.isspace() or selected == "":
         return 0
             
     front = data[row_index][0: i - len(selected)]
     back = data[row_index][i:]
-    
-    #print(front, back, data)
-    
+        
     assert len(data[row_index]) == len(front + "."*len(selected) + back)
     data[row_index] = front + "."*len(selected) + back
     
-    if False:
-        print(front, back)
-        print(row_index, char_index, data)
-        print(selected)
-        print() 
-    
-        print(selected)
     return int(selected)
+
+
+
 
 
 for i, line in enumerate(data):
@@ -66,9 +57,7 @@ for i, line in enumerate(data):
         
         if symbol == "*":
             arr = []
-            
-            #print("IJ:", i, j)
-            
+                        
             arr.append( selectnumbers(i-1, j-1))
             arr.append( selectnumbers(i-1, j))
             arr.append( selectnumbers(i-1, j+1))
@@ -81,7 +70,6 @@ for i, line in enumerate(data):
             arr.append( selectnumbers(i+1, j+1))
             
             new_arr = []
-            #print(arr)
             
             for k in arr:
                 if k != 0:
@@ -89,9 +77,7 @@ for i, line in enumerate(data):
             
             if len(new_arr) == 2:
                 sum += new_arr[0] * new_arr[1]
-                #print(new_arr[0], new_arr[1])
             
-    #print()
-
+            
 print("sum:")
 print(sum)
